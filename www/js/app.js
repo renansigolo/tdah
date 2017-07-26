a4335fe552cdb21defcfc83ab87a302f8798ff99
Renan Sigolo <renan.sigolo@gmail.com>
2017-07-25 22:43:58 -0300

    teste de caracteristicas adicionado

--- www/js/app.js
+++ www/js/app.js
@@ -86,6 +86,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
                     }
                 }
             })
+            .state('tab.diagnostico-caracteristica', {
+                url: '/chats/diagnostico-caracteristica',
+                views: {
+                    'tab-chats': {
+                        templateUrl: 'templates/diagnostico-caracteristica.html',
+                        controller: 'ChatsCtrl'
+                    }
+                }
+            })
             .state('tab.chat-detail', {
                 url: '/chats/:chatId',
                 views: {
