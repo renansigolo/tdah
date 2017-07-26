fadc503ce4659697c8829106a8dad0fd7241a0bf
Renan Sigolo <renan.sigolo@gmail.com>
2017-07-25 23:32:54 -0300

    medicacao adicionada

--- www/js/app.js
+++ www/js/app.js
@@ -95,6 +95,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
                     }
                 }
             })
+            .state('tab.medicacao', {
+                url: '/chats/medicacao',
+                views: {
+                    'tab-chats': {
+                        templateUrl: 'templates/medicacao.html',
+                        controller: 'ChatsCtrl'
+                    }
+                }
+            })
             .state('tab.chat-detail', {
                 url: '/chats/:chatId',
                 views: {
