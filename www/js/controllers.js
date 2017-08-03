2e6cd6e1b26f8a086050a774c2b9553f5cc9f825
Renan Sigolo <renan.sigolo@gmail.com>
2017-08-03 16:43:52 -0300

    sugestoes para as emocoes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -74,35 +74,67 @@ angular.module('starter.controllers', [])
             $scope.sentimentos = [
                 {
                     sentimento: 'com raiva',
-                    valor: resultado.scores.anger
+                    valor: resultado.scores.anger,
+                    sugestao: 'Para diminuir sua raiva tente as seguintes sugestões',
+                    sugestao1: 'Respire fundo durante 30 segundos',
+                    sugestao2: 'Vá dar uma volta',
+                    sugestao3: 'Tome um copa de água',
             },
                 {
                     sentimento: 'com desprezo',
-                    valor: resultado.scores.contempt
+                    valor: resultado.scores.contempt,
+                    sugestao: 'Para diminuir o desprezo sentido tente as seguintes sugestões',
+                    sugestao1: 'Pense em coisas positivas',
+                    sugestao2: 'Tente usar empatia',
+                    sugestao3: 'Pense que a situação do outro lado possa ser mais complicada do que você imagina',
             },
                 {
                     sentimento: 'com nojo',
-                    valor: resultado.scores.disgust
+                    valor: resultado.scores.disgust,
+                    sugestao: 'NOJO',
+                    sugestao1: 'P1seguintes sugestões:',
+                    sugestao2: 'P2tente as seguintes sugestões:',
+                    sugestao3: 'Pa3te as seguintes sugestões:',
             },
                 {
                     sentimento: 'com medo',
-                    valor: resultado.scores.fear
+                    valor: resultado.scores.fear,
+                    sugestao: 'Para diminuir seu medo tente as seguintes sugestões',
+                    sugestao1: 'Pense positivamente',
+                    sugestao2: 'Tente formular soluções em um cenário positivo',
+                    sugestao3: 'Fique calmo que tudo vai dar certo se você desejar e se empenhar para isso',
             },
                 {
                     sentimento: 'feliz',
-                    valor: resultado.scores.happiness
+                    valor: resultado.scores.happiness,
+                    sugestao: 'Aproveite esse momento de felicidade para',
+                    sugestao1: 'Se comunicar com as pessoas',
+                    sugestao2: 'Formular ideias para eventos futuros',
+                    sugestao3: 'Curta esse momento',
             },
                 {
                     sentimento: 'neutro(a)',
-                    valor: resultado.scores.neutral
+                    valor: resultado.scores.neutral,
+                    sugestao: 'NEUTRO',
+                    sugestao1: 'P1seguintes sugestões:',
+                    sugestao2: 'P2tente as seguintes sugestões:',
+                    sugestao3: 'Pa3te as seguintes sugestões:',
             },
                 {
                     sentimento: 'triste',
-                    valor: resultado.scores.sadness
+                    valor: resultado.scores.sadness,
+                    sugestao: 'Para diminuir sua tristeza tente as seguintes sugestões:',
+                    sugestao1: 'P1seguintes sugestões:',
+                    sugestao2: 'P2tente as seguintes sugestões:',
+                    sugestao3: 'Pa3te as seguintes sugestões:',
             },
                 {
                     sentimento: 'surpreso(a)',
-                    valor: resultado.scores.surprise
+                    valor: resultado.scores.surprise,
+                    sugestao: 'Para minimizar o estado de surpresa tente o seguinte',
+                    sugestao1: 'SURPRESA',
+                    sugestao2: 'SURPRESA',
+                    sugestao3: 'SURPRESA',
             }
         ];
 
@@ -110,6 +142,10 @@ angular.module('starter.controllers', [])
                 if ($scope.maiorEmocao <= $scope.sentimentos[i].valor) {
                     $scope.maiorEmocao = $scope.sentimentos[i].valor;
                     $scope.sentimento = $scope.sentimentos[i].sentimento;
+                    $scope.sugestao = $scope.sentimentos[i].sugestao;
+                    $scope.sugestao1 = $scope.sentimentos[i].sugestao1;
+                    $scope.sugestao2 = $scope.sentimentos[i].sugestao2;
+                    $scope.sugestao3 = $scope.sentimentos[i].sugestao3;
                 }
                 $scope.emocao = true;
             }
